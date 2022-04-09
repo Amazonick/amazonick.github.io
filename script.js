de1e3581c375fea7c7014a7344d216104a3c289a
Amazonick <pzs@live.com>
2022-04-09 16:38:35 +0200

    made CLEAR function and display input

--- script.js
+++ script.js
@@ -9,7 +9,43 @@ const minusBtn = document.getElementById('minus');
 const multiplyBtn = document.getElementById('multiply');
 const divideBtn = document.getElementById('divide');
 const equalsBtn = document.getElementById('equals');
-const numbersBtns = document.querySelectorAll('.number-btn')
+const numbersBtns = document.querySelectorAll('.number-btn');
+const clearBtn = document.getElementById('clear-btn');
+let numberBtnsValue;
+let firstNumber;
+let secondNumber;
+
+// DISPLAY NUMBERS
+function displayInput() {
+    numbersBtns.forEach((button) =>
+        button.addEventListener('click', (e) => {
+            numberBtnsValue = e.target.value;
+            calculatorDisplay.textContent += numberBtnsValue;
+        })
+    );
+}
+displayInput();
+
+// CLEAR DISPLAY
+function clearDisplay() {
+    clearBtn.addEventListener('click', () => {
+        calculatorDisplay.textContent = '';
+    });
+}
+clearDisplay();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // OPERATIONS FUNCTIONS
@@ -28,12 +64,12 @@ function division(a, b) {
 
 function operate(num1, num2, operator) {
     if (operator === '+') {
-        return addition(num1, num2)
+        return addition(num1, num2);
     } else if (operator === '-') {
-        return substraction(num1, num2)
+        return substraction(num1, num2);
     } else if (operator === '*') {
-        return multiplication(num1, num2)
+        return multiplication(num1, num2);
     } else if (operator === '/') {
-        return division(num1, num2)
+        return division(num1, num2);
     }
 }
